4ac5b2d83ed3c01357ce8e74ebb11858f4035741
IsNielsen <isnielsen13@gmail.com>
2024-11-26 02:03:05 -0700

    Organized and added comments for future additions

--- _server/registration/urls.py
+++ _server/registration/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# This is the file you want to edit
+
 urlpatterns = [
     path('sign_in/', views.sign_in),
     path('sign_up/', views.sign_up),
